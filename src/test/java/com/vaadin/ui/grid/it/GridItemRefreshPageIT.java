a0be8b76f8b63ed7d2130a14eeddc85acd02d214
Gilberto Torrezan <gilberto@vaadin.com>
2017-12-13 16:40:51 +0200

    Fix for DataProvider/Communicator refreshAll when using components.
    Depends on changes made on flow. See
    https://github.com/vaadin/flow/pull/3157

--- src/test/java/com/vaadin/ui/grid/it/GridItemRefreshPageIT.java
+++ src/test/java/com/vaadin/ui/grid/it/GridItemRefreshPageIT.java
@@ -15,6 +15,7 @@
  */
 package com.vaadin.ui.grid.it;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -31,25 +32,58 @@ import com.vaadin.flow.testutil.TestPath;
 public class GridItemRefreshPageIT extends AbstractComponentIT {
 
     @Test
-    public void updateAndRefreshItemsOnTheServer() {
-        open();
+    public void updateAndRefreshItemsOnTheServerUsingDataProvider_withTemplateRenderer() {
+        updateAndRefreshItemsOnTheServer("template-grid",
+                "template-refresh-first", "template-refresh-multiple",
+                "template-refresh-all");
+    }
 
-        WebElement grid = findElement(By.tagName("vaadin-grid"));
-        WebElement refreshFirstItem = findElement(By.id("refresh-first"));
+    @Test
+    public void updateAndRefreshItemsOnTheServerUsingDataCommunicator_withTemplateRenderer() {
+        updateAndRefreshItemsOnTheServer("template-grid",
+                "template-refresh-first-communicator",
+                "template-refresh-multiple-communicator",
+                "template-reset-communicator");
+    }
+
+    @Test
+    public void updateAndRefreshItemsOnTheServerUsingDataProvider_withComponentRenderer() {
+        updateAndRefreshItemsOnTheServer("component-grid",
+                "component-refresh-first", "component-refresh-multiple",
+                "component-refresh-all");
+    }
+
+    @Test
+    public void updateAndRefreshItemsOnTheServerUsingDataCommunicator_withComponentRenderer() {
+        updateAndRefreshItemsOnTheServer("component-grid",
+                "component-refresh-first-communicator",
+                "component-refresh-multiple-communicator",
+                "component-reset-communicator");
+    }
+
+    private void updateAndRefreshItemsOnTheServer(String gridId,
+            String refreshFirstItemButtonId,
+            String refreshMultipleItemsButtonId, String refreshAllButtonId) {
+        open();
+        WebElement grid = findElement(By.id(gridId));
+        scrollToElement(grid);
+
+        WebElement refreshFirstItem = findElement(
+                By.id(refreshFirstItemButtonId));
         WebElement refreshMultipleItems = findElement(
-                By.id("refresh-multiple"));
-        WebElement refreshAll = findElement(By.id("refresh-all"));
+                By.id(refreshMultipleItemsButtonId));
+        WebElement refreshAll = findElement(By.id(refreshAllButtonId));
 
         assertNotUpdated(grid, 0, 0);
-        refreshFirstItem.click();
+        clickElementWithJs(refreshFirstItem);
         waitUntilUpdated(grid, 0, 0);
 
         assertNotUpdated(grid, 4, 9);
-        refreshMultipleItems.click();
+        clickElementWithJs(refreshMultipleItems);
         waitUntilUpdated(grid, 4, 9);
 
         assertNotUpdated(grid, 10, 15);
-        refreshAll.click();
+        clickElementWithJs(refreshAll);
         waitUntilUpdated(grid, 10, 15);
 
         getCommandExecutor().executeScript("arguments[0].scrollToIndex(900)",
@@ -66,8 +100,8 @@ public class GridItemRefreshPageIT extends AbstractComponentIT {
                 .collect(Collectors.toSet());
         waitUntil(driver -> grid
                 .findElements(By.tagName("vaadin-grid-cell-content")).stream()
-                .map(cell -> cell.getText()).collect(Collectors.toSet())
-                .containsAll(expected));
+                .map(this::getContentIfComponentRenderered)
+                .collect(Collectors.toSet()).containsAll(expected));
     }
 
     private void assertNotUpdated(WebElement grid, int startIndex,
@@ -75,9 +109,23 @@ public class GridItemRefreshPageIT extends AbstractComponentIT {
         Set<String> expected = IntStream.range(startIndex, lastIndex + 1)
                 .mapToObj(intVal -> "updated " + String.valueOf(intVal))
                 .collect(Collectors.toSet());
-        Assert.assertFalse(grid
-                .findElements(By.tagName("vaadin-grid-cell-content")).stream()
-                .map(cell -> cell.getText()).collect(Collectors.toSet())
-                .removeAll(expected));
+        Assert.assertFalse(
+                grid.findElements(By.tagName("vaadin-grid-cell-content"))
+                        .stream().map(this::getContentIfComponentRenderered)
+                        .collect(Collectors.toSet()).removeAll(expected));
+    }
+
+    private String getContentIfComponentRenderered(WebElement cell) {
+        List<WebElement> renderer = cell
+                .findElements(By.tagName("flow-component-renderer"));
+        if (renderer.isEmpty()) {
+            return cell.getAttribute("innerHTML");
+        }
+        List<WebElement> label = renderer.get(0)
+                .findElements(By.tagName("label"));
+        if (label.isEmpty()) {
+            return "";
+        }
+        return label.get(0).getAttribute("innerHTML");
     }
 }
