f8a039baf3e40360070bcf35d9fdb017116bdd21
Gilberto Torrezan <gilberto@vaadin.com>
2018-04-09 11:46:42 +0300

    Now using the body size instead of a fixed value

--- src/test/java/com/vaadin/flow/component/grid/it/GridOnFlexLayoutIT.java
+++ src/test/java/com/vaadin/flow/component/grid/it/GridOnFlexLayoutIT.java
@@ -15,19 +15,31 @@ public class GridOnFlexLayoutIT extends AbstractComponentIT {
     @Test
     public void gridOccupies100PercentOfThePage() {
         open();
-        testBench().resizeViewPortTo(600, 600);
+        getDriver().manage().window().setSize(new Dimension(600, 600));
 
         WebElement grid = findElement(By.id("full-size-grid"));
-        Dimension dimension = grid.getSize();
-        Assert.assertEquals("The width of the grid should be 600", 600,
-                dimension.getWidth());
-        Assert.assertEquals("The height of the grid should be 600", 600, 600);
+        WebElement body = findElement(By.tagName("body"));
+        Dimension gridDimension = grid.getSize();
+        Dimension bodyDimension = body.getSize();
+        Assert.assertTrue("The body dimensions should not be 0",
+                bodyDimension.getWidth() > 0 && bodyDimension.getHeight() > 0);
 
-        testBench().resizeViewPortTo(300, 300);
-        dimension = grid.getSize();
-        Assert.assertEquals("The width of the grid should be 300", 300,
-                dimension.getWidth());
-        Assert.assertEquals("The height of the grid should be 300", 300, 300);
+        Assert.assertEquals(
+                "The width of the grid should be " + bodyDimension.getWidth(),
+                bodyDimension.getWidth(), gridDimension.getWidth());
+        Assert.assertEquals(
+                "The height of the grid should be " + bodyDimension.getHeight(),
+                bodyDimension.getHeight(), gridDimension.getHeight());
+
+        getDriver().manage().window().setSize(new Dimension(300, 300));
+        gridDimension = grid.getSize();
+        bodyDimension = body.getSize();
+        Assert.assertEquals(
+                "The width of the grid should be " + bodyDimension.getWidth(),
+                bodyDimension.getWidth(), gridDimension.getWidth());
+        Assert.assertEquals(
+                "The height of the grid should be " + bodyDimension.getHeight(),
+                bodyDimension.getHeight(), gridDimension.getHeight());
     }
 
 }
