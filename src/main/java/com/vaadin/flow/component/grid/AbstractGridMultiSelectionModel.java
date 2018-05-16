6f06aec9fdcc2d305b9f4aed4528900827582f0c
Denis Anisimov <denis@vaadin.com>
2018-05-16 11:02:21 +0300

    Correct single argument "select" method implementation
    Fixes #4111

--- src/main/java/com/vaadin/flow/component/grid/AbstractGridMultiSelectionModel.java
+++ src/main/java/com/vaadin/flow/component/grid/AbstractGridMultiSelectionModel.java
@@ -86,12 +86,12 @@ public abstract class AbstractGridMultiSelectionModel<T>
         if (isSelected(item)) {
             return;
         }
-        doSelect(item, true);
-        Set<T> selected = new HashSet<>();
-        if (item != null) {
-            selected.add(item);
+        Set<T> oldSelection = new LinkedHashSet<>(selected);
+        boolean added = selected.add(item);
+        if (added) {
+            fireSelectionEvent(new MultiSelectionEvent<>(getGrid(),
+                    getGrid().asMultiSelect(), oldSelection, true));
         }
-        doUpdateSelection(selected, Collections.emptySet(), false);
     }
 
     @Override
@@ -99,13 +99,12 @@ public abstract class AbstractGridMultiSelectionModel<T>
         if (!isSelected(item)) {
             return;
         }
-        doDeselect(item, true);
-        Set<T> deselected = new HashSet<>();
-        if (item != null) {
-            deselected.add(item);
+        Set<T> oldSelection = new LinkedHashSet<>(selected);
+        boolean removed = selected.remove(item);
+        if (removed) {
+            fireSelectionEvent(new MultiSelectionEvent<>(getGrid(),
+                    getGrid().asMultiSelect(), oldSelection, true));
         }
-        doUpdateSelection(Collections.emptySet(), deselected, false);
-        selectionColumn.setSelectAllCheckboxState(false);
     }
 
     @Override
@@ -128,7 +127,6 @@ public abstract class AbstractGridMultiSelectionModel<T>
         if (isSelected(item)) {
             return;
         }
-        doSelect(item, false);
         Set<T> selected = new HashSet<>();
         if (item != null) {
             selected.add(item);
@@ -141,7 +139,6 @@ public abstract class AbstractGridMultiSelectionModel<T>
         if (!isSelected(item)) {
             return;
         }
-        doDeselect(item, false);
         Set<T> deselected = new HashSet<>();
         if (item != null) {
             deselected.add(item);
@@ -315,24 +312,6 @@ public abstract class AbstractGridMultiSelectionModel<T>
         }
         doUpdateSelection(Collections.emptySet(), getSelectedItems(), true);
         selectionColumn.setSelectAllCheckboxState(false);
-    }
-
-    private void doSelect(T item, boolean userOriginated) {
-        Set<T> oldSelection = new LinkedHashSet<>(selected);
-        boolean added = selected.add(item);
-        if (added) {
-            fireSelectionEvent(new MultiSelectionEvent<>(getGrid(),
-                    getGrid().asMultiSelect(), oldSelection, userOriginated));
-        }
-    }
-
-    private void doDeselect(T item, boolean userOriginated) {
-        Set<T> oldSelection = new LinkedHashSet<>(selected);
-        boolean removed = selected.remove(item);
-        if (removed) {
-            fireSelectionEvent(new MultiSelectionEvent<>(getGrid(),
-                    getGrid().asMultiSelect(), oldSelection, userOriginated));
-        }
     }
 
     private void doUpdateSelection(Set<T> addedItems, Set<T> removedItems,
