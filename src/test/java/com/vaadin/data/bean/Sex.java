801b76a005df1005a5ce174b2fc544f8f9c27933
Tomi Virtanen <tltv@vaadin.com>
2018-05-28 16:43:43 +0300

    Migrated V8 TreeGridHugeTree integration test

---
+++ src/test/java/com/vaadin/data/bean/Sex.java
@@ -0,0 +1,20 @@
+package com.vaadin.data.bean;
+
+public enum Sex {
+    MALE("Male"), FEMALE("Female"), UNKNOWN("Unknown");
+
+    private String stringRepresentation;
+
+    private Sex(String stringRepresentation) {
+        this.stringRepresentation = stringRepresentation;
+    }
+
+    public String getStringRepresentation() {
+        return stringRepresentation;
+    }
+
+    @Override
+    public String toString() {
+        return getStringRepresentation();
+    }
+}
