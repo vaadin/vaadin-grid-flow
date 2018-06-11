6f02249f1365cb92e60aff8cf9cc90e3930345d2
Tomi Virtanen <tltv@vaadin.com>
2018-06-11 15:42:46 +0300

    Fixed typo in @Route path name

--- src/test/java/com/vaadin/flow/component/treegrid/it/TreeGridItemDetailsRendererIT.java
+++ src/test/java/com/vaadin/flow/component/treegrid/it/TreeGridItemDetailsRendererIT.java
@@ -21,7 +21,7 @@ import org.junit.Test;
 
 import com.vaadin.flow.testutil.TestPath;
 
-@TestPath("treegrid-item-details-rederer")
+@TestPath("treegrid-item-details-renderer")
 public class TreeGridItemDetailsRendererIT extends AbstractTreeGridIT {
 
     @Before
