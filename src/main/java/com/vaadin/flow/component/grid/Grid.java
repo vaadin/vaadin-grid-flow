dd2b764019f3bd3d561215af04e655adf17a7552
Denis Anisimov <denis@vaadin.com>
2018-11-08 17:14:31 +0300

    Reuse visible accessors from Component and add javadocs to column
    Don't override get/setVisible methods (they have special handling in the
    default implementation) and add javadocs for add column methods which
    metnion sending column related data to the client side.
    
    Fixes #117

--- src/main/java/com/vaadin/flow/component/grid/Grid.java
+++ src/main/java/com/vaadin/flow/component/grid/Grid.java
@@ -255,6 +255,13 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
     /**
      * Server-side component for the {@code <vaadin-grid-column>} element.
      *
+     * <p>
+     * Every added column sends data to the client side regardless of its
+     * visibility state. Don't add a new column at all or use
+     * {@link Grid#removeColumn(Column)} to avoid sending extra data if you want
+     * to hide the column and optimize data communication.
+     * </p>
+     *
      * @param <T>
      *            type of the underlying grid this column is compatible with
      */
@@ -1246,12 +1253,20 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      * <em>NOTE:</em> For displaying components, see
      * {@link #addComponentColumn(ValueProvider)}. For using build-in renderers,
      * see {@link #addColumn(Renderer)}.
+     * </p>
+     * <p>
+     * Every added column sends data to the client side regardless of its
+     * visibility state. Don't add a new column at all or use
+     * {@link Grid#removeColumn(Column)} to avoid sending extra data if you want
+     * to hide the column and optimize data communication.
+     * </p>
      *
      * @param valueProvider
      *            the value provider
      * @return the created column
      * @see #addComponentColumn(ValueProvider)
      * @see #addColumn(Renderer)
+     * @see #removeColumn(Column)
      */
     public Column<T> addColumn(ValueProvider<T, ?> valueProvider) {
         String columnId = createColumnId(false);
@@ -1279,6 +1294,13 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      * <p>
      * <em>NOTE:</em> Using {@link ComponentRenderer} is not as efficient as the
      * built in renderers or using {@link TemplateRenderer}.
+     * </p>
+     * <p>
+     * Every added column sends data to the client side regardless of its
+     * visibility state. Don't add a new column at all or use
+     * {@link Grid#removeColumn(Column)} to avoid sending extra data if you want
+     * to hide the column and optimize data communication.
+     * </p>
      *
      * @param componentProvider
      *            a value provider that will return a component for the given
@@ -1287,6 +1309,7 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      *            the component type
      * @return the new column
      * @see #addColumn(Renderer)
+     * @see #removeColumn(Column)
      */
     public <V extends Component> Column<T> addComponentColumn(
             ValueProvider<T, V> componentProvider) {
@@ -1301,8 +1324,16 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      * automatically configured using the return type of the given
      * {@link ValueProvider}.
      *
+     * <p>
+     * Every added column sends data to the client side regardless of its
+     * visibility state. Don't add a new column at all or use
+     * {@link Grid#removeColumn(Column)} to avoid sending extra data if you want
+     * to hide the column and optimize data communication.
+     * </p>
+     *
      * @see Column#setComparator(ValueProvider)
      * @see Column#setSortProperty(String...)
+     * @see #removeColumn(Column)
      *
      * @param valueProvider
      *            the value provider
@@ -1329,6 +1360,13 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      * {@link #addComponentColumn(ValueProvider)}, but using
      * {@link ComponentRenderer} is not as efficient as the built in renderers
      * or using {@link TemplateRenderer}.
+     * </p>
+     * <p>
+     * Every added column sends data to the client side regardless of its
+     * visibility state. Don't add a new column at all or use
+     * {@link Grid#removeColumn(Column)} to avoid sending extra data if you want
+     * to hide the column and optimize data communication.
+     * </p>
      *
      * @param renderer
      *            the renderer used to create the grid cell structure
@@ -1336,6 +1374,7 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      *
      * @see TemplateRenderer#of(String)
      * @see #addComponentColumn(ValueProvider)
+     * @see #removeColumn(Column)
      */
     public Column<T> addColumn(Renderer<T> renderer) {
         String columnId = createColumnId(true);
@@ -1393,6 +1432,15 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      * sorting property does not extend Comparable, no in-memory sorting is
      * configured for it.
      *
+     * <p>
+     * Every added column sends data to the client side regardless of its
+     * visibility state. Don't add a new column at all or use
+     * {@link Grid#removeColumn(Column)} to avoid sending extra data if you want
+     * to hide the column and optimize data communication.
+     * </p>
+     *
+     * @see #removeColumn(Column)
+     *
      * @param renderer
      *            the renderer used to create the grid cell structure
      * @param sortingProperties
@@ -1447,6 +1495,15 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      * <p>
      * <strong>Note:</strong> This method can only be used for a Grid created
      * from a bean type with {@link #Grid(Class)}.
+     *
+     * <p>
+     * Every added column sends data to the client side regardless of its
+     * visibility state. Don't add a new column at all or use
+     * {@link Grid#removeColumn(Column)} to avoid sending extra data if you want
+     * to hide the column and optimize data communication.
+     * </p>
+     *
+     * @see #removeColumn(Column)
      *
      * @param propertyName
      *            the property name of the new column, not <code>null</code>
@@ -1506,9 +1563,17 @@ public class Grid<T> extends Component implements HasDataProvider<T>, HasStyle,
      * <strong>Note:</strong> This method can only be used for a Grid created
      * from a bean type with {@link #Grid(Class)}.
      *
+     * <p>
+     * Every added column sends data to the client side regardless of its
+     * visibility state. Don't add a new column at all or use
+     * {@link Grid#removeColumn(Column)} to avoid sending extra data if you want
+     * to hide the column and optimize data communication.
+     * </p>
+     *
      * @param propertyNames
      *            the property names of the new columns, not <code>null</code>
      * @see #addColumn(String)
+     * @see #removeColumn(Column)
      */
     public void addColumns(String... propertyNames) {
         checkForBeanGrid();
