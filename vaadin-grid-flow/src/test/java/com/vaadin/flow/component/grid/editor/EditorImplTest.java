ce10445625bd6c60b578ebdf46d71cf12eba150f
Klaudeta <klaudeta@vaadin.com>
2019-02-08 15:04:39 +0200

    fix editor test due to changes on editor impl

--- vaadin-grid-flow/src/test/java/com/vaadin/flow/component/grid/editor/EditorImplTest.java
+++ vaadin-grid-flow/src/test/java/com/vaadin/flow/component/grid/editor/EditorImplTest.java
@@ -18,6 +18,8 @@ package com.vaadin.flow.component.grid.editor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -25,13 +27,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.editor.EditorEvent;
-import com.vaadin.flow.component.grid.editor.EditorImpl;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.StatusChangeEvent;
 import com.vaadin.flow.function.ValueProvider;
+import com.vaadin.flow.server.VaadinRequest;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinSession;
 
 public class EditorImplTest {
 
@@ -40,6 +44,7 @@ public class EditorImplTest {
 
     private Grid<String> grid;
     private TestEditor editor;
+	private MockUI ui;
 
     private static class TestEditor extends EditorImpl<String> {
 
@@ -55,9 +60,17 @@ public class EditorImplTest {
         }
     }
 
+	private void fakeClientCommunication() {
+		ui.getInternals().getStateTree().runExecutionsBeforeClientResponse();
+		ui.getInternals().getStateTree().collectChanges(ignore -> {
+		});
+	}
+
     @Before
     public void setUp() {
         grid = new Grid<>();
+		ui = new MockUI();
+		ui.getElement().appendChild(grid.getElement());
         editor = new TestEditor(grid);
 
         editor.setBinder(new Binder<>());
@@ -67,12 +80,14 @@ public class EditorImplTest {
     @Test(expected = IllegalStateException.class)
     public void editItem_itemIsNotKnown_throw() {
         editor.editItem("foo");
+		fakeClientCommunication();
     }
 
     @Test(expected = IllegalStateException.class)
     public void editItem_noBinder_throw() {
         editor = new TestEditor(grid);
         editor.editItem("bar");
+		fakeClientCommunication();
     }
 
     @Test(expected = IllegalStateException.class)
@@ -81,14 +96,15 @@ public class EditorImplTest {
 
         editor.setBuffered(true);
         editor.editItem("bar");
-
+		fakeClientCommunication();
         editor.editItem("foo");
+		fakeClientCommunication();
     }
 
     @Test
     public void editItem_itemIsKnown_binderStatusEventAndEditorOpenEvent() {
         AtomicReference<StatusChangeEvent> statusEventCapture = new AtomicReference<>();
-        AtomicReference<EditorEvent<String>> openEventCapure = new AtomicReference<EditorEvent<String>>();
+        AtomicReference<EditorEvent<String>> openEventCapure = new AtomicReference<>();
         assertOpenEvents(statusEventCapture, openEventCapure);
 
         // In not buffered mode there is the bean in the binder
@@ -100,7 +116,7 @@ public class EditorImplTest {
         editor.setBuffered(true);
 
         AtomicReference<StatusChangeEvent> statusEventCapture = new AtomicReference<>();
-        AtomicReference<EditorEvent<String>> openEventCapure = new AtomicReference<EditorEvent<String>>();
+        AtomicReference<EditorEvent<String>> openEventCapure = new AtomicReference<>();
         assertOpenEvents(statusEventCapture, openEventCapure);
 
         // In not buffered mode there is no bean in the binder
@@ -112,8 +128,13 @@ public class EditorImplTest {
         grid.getDataCommunicator().getKeyMapper().key("foo");
 
         editor.editItem("bar");
+
+		fakeClientCommunication();
+
         editor.refreshedItems.clear();
         editor.editItem("foo");
+
+		fakeClientCommunication();
 
         Assert.assertEquals(2, editor.refreshedItems.size());
         Assert.assertEquals("bar", editor.refreshedItems.get(0));
@@ -123,6 +144,7 @@ public class EditorImplTest {
     @Test
     public void cancel_eventIsFiredAndItemIsRefreshed() {
         editor.editItem("bar");
+		fakeClientCommunication();
         editor.refreshedItems.clear();
 
         AtomicReference<EditorEvent<String>> cancelEventCapture = new AtomicReference<>();
@@ -158,6 +180,7 @@ public class EditorImplTest {
     @Test
     public void save_editorIsOpened_editorIsInNotBufferedMode_noEvents() {
         editor.editItem("bar");
+		fakeClientCommunication();
         editor.refreshedItems.clear();
 
         AtomicReference<StatusChangeEvent> statusEventCapture = new AtomicReference<>();
@@ -176,6 +199,7 @@ public class EditorImplTest {
     @Test
     public void save_editorIsOpened_editorIsInBufferedMode_eventsAreFired() {
         editor.editItem("bar");
+		fakeClientCommunication();
         editor.refreshedItems.clear();
         editor.setBuffered(true);
 
@@ -200,6 +224,7 @@ public class EditorImplTest {
                 .bind(ValueProvider.identity(), (item, value) -> {
                 });
         editor.editItem("bar");
+		fakeClientCommunication();
         editor.refreshedItems.clear();
         editor.setBuffered(true);
 
@@ -224,6 +249,7 @@ public class EditorImplTest {
         thrown.reportMissingExceptionWithMessage("Buffered editor should be closed using save() or cancel()");
 
         editor.editItem("bar");
+		fakeClientCommunication();
         editor.refreshedItems.clear();
         editor.setBuffered(true);
 
@@ -240,6 +266,7 @@ public class EditorImplTest {
     @Test
     public void editorInUnBufferedMode_closeEditorSendsCloseEvent() {
         editor.editItem("bar");
+		fakeClientCommunication();
         editor.refreshedItems.clear();
 
         AtomicReference<EditorEvent<String>> closeEventCapture = new AtomicReference<>();
@@ -248,7 +275,6 @@ public class EditorImplTest {
                 event -> closeEventCapture.compareAndSet(null, event));
 
         editor.closeEditor();
-
         Assert.assertNotNull("No close event was fired.", closeEventCapture.get());
     }
 
@@ -284,10 +310,92 @@ public class EditorImplTest {
         editor.addOpenListener(
                 event -> openEventCapure.compareAndSet(null, event));
         editor.editItem("bar");
-
+		fakeClientCommunication();
         Assert.assertNotNull(statusEventCapture.get());
         Assert.assertNotNull(openEventCapure.get());
 
         Assert.assertEquals("bar", openEventCapure.get().getItem());
     }
+
+	public static class MockUI extends UI {
+
+		public MockUI() {
+			this(findOrcreateSession());
+		}
+
+		public MockUI(VaadinSession session) {
+			getInternals().setSession(session);
+			setCurrent(this);
+		}
+
+		@Override
+		protected void init(VaadinRequest request) {
+			// Do nothing
+		}
+
+		private static VaadinSession findOrcreateSession() {
+			VaadinSession session = VaadinSession.getCurrent();
+			if (session == null) {
+				session = new AlwaysLockedVaadinSession(null);
+				VaadinSession.setCurrent(session);
+			}
+			return session;
+		}
+	}
+
+	public static class AlwaysLockedVaadinSession extends MockVaadinSession {
+
+		public AlwaysLockedVaadinSession(VaadinService service) {
+			super(service);
+			lock();
+		}
+
+	}
+
+	public static class MockVaadinSession extends VaadinSession {
+		/*
+		 * Used to make sure there's at least one reference to the mock session while
+		 * it's locked. This is used to prevent the session from being eaten by GC in
+		 * tests where @Before creates a session and sets it as the current instance
+		 * without keeping any direct reference to it. This pattern has a chance of
+		 * leaking memory if the session is not unlocked in the right way, but it should
+		 * be acceptable for testing use.
+		 */
+		private static final ThreadLocal<MockVaadinSession> referenceKeeper = new ThreadLocal<>();
+
+		public MockVaadinSession(VaadinService service) {
+			super(service);
+		}
+
+		@Override
+		public void close() {
+			super.close();
+			closeCount++;
+		}
+
+		public int getCloseCount() {
+			return closeCount;
+		}
+
+		@Override
+		public Lock getLockInstance() {
+			return lock;
+		}
+
+		@Override
+		public void lock() {
+			super.lock();
+			referenceKeeper.set(this);
+		}
+
+		@Override
+		public void unlock() {
+			super.unlock();
+			referenceKeeper.remove();
+		}
+
+		private int closeCount;
+
+		private ReentrantLock lock = new ReentrantLock();
+	}
 }
