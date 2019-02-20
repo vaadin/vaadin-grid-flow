5d3ca2fa51a4d261223df62f1532257de34ef0e8
Reuben Sivan <rsivan@advaoptical.com>
2019-02-20 09:54:51 -0800

    remove unused import

--- vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridMultiSelectionColumnPage.java
+++ vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridMultiSelectionColumnPage.java
@@ -17,7 +17,6 @@ package com.vaadin.flow.component.grid.it;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
