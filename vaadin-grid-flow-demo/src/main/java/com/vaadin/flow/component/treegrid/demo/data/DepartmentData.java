fdf3c423a927e5cf7bab7a28d9cbae1c76a1f352
shadikhani <shadi@vaadin.com>
2019-04-15 20:25:27 +0300

    New TreeGrid demo changes

---
+++ vaadin-grid-flow-demo/src/main/java/com/vaadin/flow/component/treegrid/demo/data/DepartmentData.java
@@ -0,0 +1,46 @@
+package com.vaadin.flow.component.treegrid.demo.data;
+
+import com.vaadin.flow.component.treegrid.demo.entity.Department;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class DepartmentData {
+    private List<Department> list = new ArrayList<>();
+
+    public DepartmentData() {
+
+        list.add(new Department(1, "Product Development", null, "Manager is Paivi"));
+        list.add(new Department(11, "Flow", list.get(0), "Direct manager is Pekka"));
+        list.add(new Department(111, "Flow Core", list.get(1), "Direct manager is Pekka"));
+        list.add(new Department(111, "Flow Components", list.get(1), "Direct manager is Gilberto"));
+        list.add(new Department(12, "Design", list.get(0), "Direct manager is Pekka"));
+        list.add(new Department(13, "DJO", list.get(0), "Direct manager is Thomas"));
+        list.add(new Department(14, "Component", list.get(0), "Direct manager is Tomi"));
+        list.add(new Department(2, "HR", null, "Direct manager is Anne"));
+        list.add(new Department(21, "Office", list.get(7), "Direct manager is Anu"));
+        list.add(new Department(22, "Employee", list.get(7), "Direct manager is Minnah"));
+        list.add(new Department(3, "Marketing", null, "Manager Niko van Eeghen"));
+        list.add(new Department(31, "Growth", list.get(10), "Direct manager is Ömer Tümer"));
+        list.add(new Department(32, "Demand Generation", list.get(10), "Direct manager is Marcus"));
+        list.add(new Department(33, "Product Marketing", list.get(10), "Direct manager is Pekka"));
+        list.add(new Department(34, "Brand Experience", list.get(10), "Direct manager is Eero"));
+
+    }
+
+    public List<Department> getDepartments() {
+        return list;
+    }
+
+    public List<Department> getRootDepartments() {
+        return list.stream().filter(department -> department.getParent() == null)
+                .collect(Collectors.toList());
+    }
+
+    public List<Department> getChildDepartments(Department parent) {
+        return list.stream().filter(department -> Objects.equals(department.getParent(), parent))
+                .collect(Collectors.toList());
+    }
+}
---
+++ vaadin-grid-flow-demo/src/main/java/com/vaadin/flow/component/treegrid/demo/entity/Department.java
@@ -0,0 +1,53 @@
+package com.vaadin.flow.component.treegrid.demo.entity;
+
+public class Department {
+    private int id;
+    private String name;
+    private String description;
+    private Department parent;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Department getParent() {
+        return parent;
+    }
+
+    public void setParent(Department parent) {
+        this.parent = parent;
+    }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Department(int id, String name, Department department, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.parent = department;
+    }
+}
