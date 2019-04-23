c1cacee10173b9541ae8640bc35db8c2c7fb2d11
Kari Söderholm <haprog@vaadin.com>
2019-04-23 18:00:06 +0300

    fix test

--- vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/ColumnAutoWidthIT.java
+++ vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/ColumnAutoWidthIT.java
@@ -41,14 +41,49 @@ public class ColumnAutoWidthIT extends AbstractComponentIT {
                 grid));
     }
 
+    /**
+     * Converts a CSS pixel value string to an integer. Assumes that the given
+     * string is a CSS value in pixels (e.g. "100px").
+     * 
+     * @param cssValue
+     *            Css pixel value (e.g. "100px")
+     * @return Number of pixels as an integer
+     */
+    private int cssPixelValueToInteger(String cssValue) {
+        return Integer.parseInt(cssValue.replaceFirst("px", ""));
+    }
+
+    /**
+     * Assert that the given CSS pixel value is close to the expected value
+     * (with a margin of ±<code>delta</code>)
+     * 
+     * @param expected
+     *            expected value
+     * @param cssValue
+     *            CSS pixel value
+     * @param delta
+     *            allowed margin of deviation from the expected value
+     */
+    private void assertCssPixelValueCloseTo(int expected, String cssValue,
+            int delta) {
+        int value = cssPixelValueToInteger(cssValue);
+        int min = expected - delta;
+        int max = expected + delta;
+        boolean valueInAllowedRange = (min <= value) && (value <= max);
+        Assert.assertTrue(
+                "CSS value '" + cssValue + "' does not match the expected "
+                        + expected + " (±" + delta + ") pixels.",
+                valueInAllowedRange);
+    }
+
     @Test
     public void columnWidthsAreSetCorrectly() {
         List<String> colWidths = (List<String>) executeScript(
                 "return arguments[0]._getColumns().map(col => col.width)",
                 grid);
 
-        Assert.assertEquals("55px", colWidths.get(0));
-        Assert.assertEquals("432px", colWidths.get(2));
-        Assert.assertEquals("256px", colWidths.get(3));
+        assertCssPixelValueCloseTo(55, colWidths.get(0), 5);
+        assertCssPixelValueCloseTo(420, colWidths.get(2), 5);
+        assertCssPixelValueCloseTo(243, colWidths.get(3), 5);
     }
 }
