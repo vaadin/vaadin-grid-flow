987f983253dcf25f4c096c358c9c62c205e972f0
Manolo Carrasco <manolo@apache.org>
2019-05-02 11:08:30 +0200

    Remove comments

--- vaadin-grid-flow-integration-tests/frontend/src/grid-in-a-template.js
+++ vaadin-grid-flow-integration-tests/frontend/src/grid-in-a-template.js
@@ -1,10 +1,3 @@
-/* Dependency resources */
-/* Defines the example-template element */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
