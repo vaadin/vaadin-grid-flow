2a514c970a9fd85164b2fef12f5aec44f4c3b8e6
Martin Vysny <martin@vysny.me>
2019-09-05 16:41:04 +0300

    Minor: removed unnecessary imports

--- vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
+++ vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
@@ -22,7 +22,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
