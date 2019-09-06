e9c32ef38f4f91e90598f07a8cdcb591918a0534
Martin Vysny <martin@vysny.me>
2019-09-06 10:55:58 +0300

    Fixed imports

--- vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/GridColumnOrderHelper.java
+++ vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/GridColumnOrderHelper.java
@@ -17,7 +17,17 @@ package com.vaadin.flow.component.grid;
 
 import com.vaadin.flow.component.Component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
