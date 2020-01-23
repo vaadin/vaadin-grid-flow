84e7520776444637303a416dfaec2d7793aeb32d
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-01-23 11:51:37 +0200

    add test for autoWidth of flow-selection-column is true

--- vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridMultiSelectionColumnPageIT.java
+++ vaadin-grid-flow-integration-tests/src/test/java/com/vaadin/flow/component/grid/it/GridMultiSelectionColumnPageIT.java
@@ -153,4 +153,15 @@ public class GridMultiSelectionColumnPageIT extends AbstractComponentIT {
                 "All selection shouldn't be possible when the selectAll is hidden",
                 "", message.getText());
     }
+
+    @Test
+    public void shouldAutoWidthOfSelectionColumnIsTrue() {
+        open();
+        WebElement grid = findElement(By.id("in-memory-grid"));
+
+        // Test autoWidth of selection column is true
+        WebElement gridSelectionMode = grid.findElement(By.tagName("vaadin-grid-flow-selection-column"));
+        String autoWidth = gridSelectionMode.getAttribute("autoWidth");
+        Assert.assertTrue("autoWidth  should be true", Boolean.parseBoolean(autoWidth));
+    }
 }
