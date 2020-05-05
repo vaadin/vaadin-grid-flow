3b94be2846788f9b3684475007fc7d4035052e38
Tomi Virkki <virkki@vaadin.com>
2020-05-05 10:22:35 +0300

    Use stable Web Component version

--- vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
+++ vaadin-grid-flow/src/main/java/com/vaadin/flow/component/grid/Grid.java
@@ -124,7 +124,7 @@ import elemental.json.JsonValue;
  *
  */
 @Tag("vaadin-grid")
-@NpmPackage(value = "@vaadin/vaadin-grid", version = "5.6.0-beta1")
+@NpmPackage(value = "@vaadin/vaadin-grid", version = "5.6.0")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid.js")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid-column.js")
 @JsModule("@vaadin/vaadin-grid/src/vaadin-grid-sorter.js")
