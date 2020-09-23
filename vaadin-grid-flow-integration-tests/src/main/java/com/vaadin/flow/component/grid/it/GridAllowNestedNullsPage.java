88d52cd5aaa70e8a5842a59f12ce0d2f685aa759
Tatu Lund <tatu@vaadin.com>
2020-09-23 15:41:49 +0300

    Added missing import

--- vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridAllowNestedNullsPage.java
+++ vaadin-grid-flow-integration-tests/src/main/java/com/vaadin/flow/component/grid/it/GridAllowNestedNullsPage.java
@@ -18,6 +18,7 @@ package com.vaadin.flow.component.grid.it;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.NestedNullBehavior;
 import com.vaadin.flow.component.html.Div;
